250c8e41bc87588fcb822dc88c75bef0f297eace
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-12-23 17:20:36 +0530

    Remove loop in Linked List
    Remove loop in Linked List 
    Medium Accuracy: 47.96% Submissions: 100k+ Points: 4
    Given a linked list of N nodes such that it may contain a loop.
    
    A loop here means that the last node of the link list is connected to the node at position X. If the link list does not have any loop, X=0.
    
    Remove the loop from the linked list, if it is present.  
    
    
    Example 1:
    
    Input:
    N = 3
    value[] = {1,3,4}
    X = 2
    Output: 1
    Explanation: The link list looks like
    1 -> 3 -> 4
         ^    |
         |____|    
    A loop is present. If you remove it 
    successfully, the answer will be 1. 
    
    Example 2:
    
    Input:
    N = 4
    value[] = {1,8,3,4}
    X = 0
    Output: 1
    Explanation: The Linked list does not 
    contains any loop. 
    
    Example 3:
    
    Input:
    N = 4
    value[] = {1,2,3,4}
    X = 1
    Output: 1
    Explanation: The link list looks like 
    1 -> 2 -> 3 -> 4
    |______________|
    A loop is present. 
    If you remove it successfully, 
    the answer will be 1. 
    
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function removeLoop() which takes the head of the linked list as the input parameter. Simply remove the loop in the list (if present) without disconnecting any nodes from the list.
    Note: The generated output will be 1 if your submitted code is correct.
    
    
    Expected time complexity: O(N)
    Expected auxiliary space: O(1)
    
    
    Constraints:
    1 ≤ N ≤ 104

--- Remove loop in Linked List.cpp
+++ Remove loop in Linked List.cpp
@@ -1,3 +1,50 @@
+class Solution
+{
+    public:
+    //Function to remove a loop in the linked list.
+    void removeLoop(Node* head)
+    {
+        if(head==NULL)
+        return;
+        Node* fast=head->next;
+        Node* slow=head;
+        
+        while(fast!=slow)
+        {
+            if(fast==NULL || fast->next==NULL)
+            return;
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        
+        int size=1;
+        fast=fast->next;
+        
+        while(fast!=slow)
+        {
+            size++;
+            fast=fast->next;
+        }
+        slow=head;
+        fast=head;
+        
+        for(int i=0;i<size-1;i++)
+        fast=fast->next;
+        
+        while(fast->next!=slow)
+        {
+            slow=slow->next;
+            fast=fast->next;
+        }
+        
+        fast->next=NULL;
+        
+        // code here
+        // just remove the loop without losing any nodes
+    }
+};
+
+
 #include <unordered_set>
 class Solution
 {
