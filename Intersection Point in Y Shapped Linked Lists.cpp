f2c0365b6f6e8971f800c6ed28a9187e2998dd4e
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-12-23 17:40:53 +0530

    Intersection Point in Y Shapped Linked Lists
    Intersection Point in Y Shapped Linked Lists 
    Medium Accuracy: 49.55% Submissions: 100k+ Points: 4
    Given two singly linked lists of size N and M, write a program to get the point where two linked lists intersect each other.
    
     
    
    Example 1:
    
    Input:
    LinkList1 = 3->6->9->common
    LinkList2 = 10->common
    common = 15->30->NULL
    Output: 15
    Explanation:
    Y ShapedLinked List
    Example 2:
    
    Input: 
    Linked List 1 = 4->1->common
    Linked List 2 = 5->6->1->common
    common = 8->4->5->NULL
    Output: 8
    Explanation: 
    
    4              5
    |              |
    1              6
     \             /
      8   -----  1 
       |
       4
       |
      5
      |
      NULL       
    Your Task:
    You don't need to read input or print anything. The task is to complete the function intersetPoint() which takes the pointer to the head of linklist1(head1) and linklist2(head2) as input parameters and returns data value of a node where two linked lists intersect. If linked list do not merge at any point, then it should return -1.
    Challenge : Try to solve the problem without using any extra space.
    
     
    
    Expected Time Complexity: O(N+M)
    Expected Auxiliary Space: O(1)
    
     
    
    Constraints:
    1 ≤ N + M ≤ 2*105
    -1000 ≤ value ≤ 1000

--- Intersection Point in Y Shapped Linked Lists.cpp
+++ Intersection Point in Y Shapped Linked Lists.cpp
@@ -1,3 +1,48 @@
+int length(Node * head)
+{
+    int size=0;
+    
+    while(head!=NULL)
+    {
+        size++;
+        head=head->next;
+    }
+    return size;
+}
+
+Node* trav(Node* head,int len)
+{
+    while(len>0)
+    {
+        head=head->next;
+        len--;
+    }
+    return head;
+}
+
+
+//Function to find intersection point in Y shaped Linked Lists.
+int intersectPoint(Node* head1, Node* head2)
+{
+    int c1=length(head1);
+    int c2=length(head2);
+    
+    if(c1>c2)
+    head1=trav(head1,c1-c2);
+    else
+    head2=trav(head2,c2-c1);
+    
+    
+    while(head1!=NULL && head2!=NULL && head1!=head2)
+    {
+        head1=head1->next;
+        head2=head2->next;
+    }
+    if(head1!=NULL)
+    return head1->data;
+    return -1;
+    // Your Code Here
+}
 
 
 
