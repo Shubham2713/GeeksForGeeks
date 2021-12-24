ccdb882a04a3bbc8926f681e9a8ad2f6a7111bfc
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-12-24 23:15:24 +0530

    Flattening a Linked List
    Flattening a Linked List 
    Medium Accuracy: 33.91% Submissions: 72353 Points: 4
    Given a Linked List of size N, where every node represents a sub-linked-list and contains two pointers:
    (i) a next pointer to the next node,
    (ii) a bottom pointer to a linked list where this node is head.
    Each of the sub-linked-list is in sorted order.
    Flatten the Link List such that all the nodes appear in a single level while maintaining the sorted order. 
    Note: The flattened list will be printed using the bottom pointer instead of next pointer.
    
     
    
    Example 1:
    
    Input:
    5 -> 10 -> 19 -> 28
    |     |     |     | 
    7     20    22   35
    |           |     | 
    8          50    40
    |                 | 
    30               45
    Output:  5-> 7-> 8- > 10 -> 19-> 20->
    22-> 28-> 30-> 35-> 40-> 45-> 50.
    Explanation:
    The resultant linked lists has every 
    node in a single level.
    (Note: | represents the bottom pointer.)
     
    
    Example 2:
    
    Input:
    5 -> 10 -> 19 -> 28
    |          |                
    7          22   
    |          |                 
    8          50 
    |                           
    30              
    Output: 5->7->8->10->19->20->22->30->50
    Explanation:
    The resultant linked lists has every
    node in a single level.
    
    (Note: | represents the bottom pointer.)
     
    
    Your Task:
    You do not need to read input or print anything. Complete the function flatten() that takes the head of the linked list as input parameter and returns the head of flattened link list.
    
     
    
    Expected Time Complexity: O(N*M)
    Expected Auxiliary Space: O(1)
    
     
    
    Constraints:
    0 <= N <= 50
    1 <= Mi <= 20
    1 <= Element of linked list <= 103

--- Flattening a Linked List.cpp
+++ Flattening a Linked List.cpp
@@ -1,3 +1,39 @@
+Node* merge(Node * a, Node* b)
+{
+    if(a==NULL)
+    return b;
+    if(b==NULL)
+    return a;
+    Node* res;
+    if(a->data<b->data)
+    {
+        res=a;
+        res->bottom=merge(a->bottom,b);
+        
+    }
+    else
+    {
+        res=b;
+        res->bottom=merge(a,b->bottom);
+    }
+    return res;
+}
+
+
+/*  Function which returns the  root of 
+    the flattened linked list. */
+Node *flatten(Node *root)
+{
+    if(root==NULL || root->next==NULL)
+    return root;
+    
+    return merge(root,flatten(root->next));
+   // Your code here
+}
+
+
+
+
 Node *flatten(Node *root)
 {
     if(root==NULL)
