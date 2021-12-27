54c351fd6c22765b2cedade512f7d59b63396564
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-12-27 11:35:07 +0530

    Number of paths
    Number of paths 
    Easy Accuracy: 57.18% Submissions: 18493 Points: 2
    The problem is to count all the possible paths from top left to bottom right of a MxN matrix with the constraints that from each cell you can either move to right or down.
    
    Example 1:
    
    Input:
    M = 3 and N = 3
    Output: 6
    Explanation:
    Let the given input 3*3 matrix is filled 
    as such:
    A B C
    D E F
    G H I
    The possible paths which exists to reach 
    'I' from 'A' following above conditions 
    are as follows:ABCFI, ABEHI, ADGHI, ADEFI, 
    ADEHI, ABEFI
     
    
    Example 2:
    
    Input:
    M = 2 and N = 8
    Output: 8
    
    Your Task:  
    You don't need to read input or print anything. Your task is to complete the function numberOfPaths() which takes the integer M and integer N as input parameters and returns the number of paths..
    
    Expected Time Complexity: O(m + n - 1))
    Expected Auxiliary Space: O(1)
    
     
    
    Constraints:
    1 ≤ M, N ≤ 10

--- Number of paths.cpp
+++ Number of paths.cpp
@@ -1,5 +1,22 @@
 long long  numberOfPaths(int m, int n)
 {
+    int dp[m][n]={0};
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i==0 || j==0)
+            dp[i][j]=1;
+            else
+            dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        }
+    }
+    return dp[m-1][n-1];
+    // Code Here
+}
+
+long long  numberOfPaths(int m, int n)
+{
     // Code Here
     if(m==1 || n==1)
     return 1;
